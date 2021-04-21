99e745b91c1e386897f85e7775839fcd870ece58
dongcy <dongcy@ickey.cn>
2021-04-21 13:39:28 +0800

    加入外部包
    Change-Id: I7e0a8303955c39f277c597ec5140e6b068a83806

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package test_go_mod
 
 import (
     "fmt"
+    "github.com/jehiah/go-strftime"
+    "time"
 )
 
 func Print2222() {
     fmt.Println("222222")
+    strftime.Format("", time.Now())
 }
